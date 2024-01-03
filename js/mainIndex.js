2d252f1e210644dffec3ee090e42af3a193abeb5
matempong <>
2024-01-04 07:50:59 +0900

    upload

---
+++ js/mainIndex.js
@@ -0,0 +1,7 @@
+const $sideMenu = document.querySelector('.sideMenu')
+const $blackOut = document.querySelector('.blackout');
+const $gnb = document.querySelector('.gnb');
+
+$blackOut.addEventListener('click', function(){
+
+})
