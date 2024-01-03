6f46996da3e18b32ec8d15b01b54cb130e9a543f
matempong <>
2024-01-04 08:16:40 +0900

    update

--- js/mainIndex.js
+++ js/mainIndex.js
@@ -6,6 +6,10 @@ const $gnb = document.querySelector('.gnb');
 
 // 햄버거매뉴 클릭 시 aside형식으로 나오게 만들기
 $hamburger.addEventListener('click', function(){
-    console.log(1);
+    // console.log(1);
     $sideMenu.classList.add('open')
+    
+    setTimeout(() => {
+        $blackOut.classList.add('open')
+    }, 750);
 })
