bfdefdaef9c7014f98bf4c48df5148aa3a0014c2
matempong <>
2024-01-04 08:05:43 +0900

    toggle

--- js/mainIndex.js
+++ js/mainIndex.js
@@ -1,7 +1,11 @@
-const $sideMenu = document.querySelector('.sideMenu')
+
+const $hamburger = document.querySelector('.hamburger');
+const $sideMenu = document.querySelector('.sideMenu');
 const $blackOut = document.querySelector('.blackout');
 const $gnb = document.querySelector('.gnb');
 
-$blackOut.addEventListener('click', function(){
-
+// 햄버거매뉴 클릭 시 aside형식으로 나오게 만들기
+$hamburger.addEventListener('click', function(){
+    console.log(1);
+    $sideMenu.classList.add('open')
 })
