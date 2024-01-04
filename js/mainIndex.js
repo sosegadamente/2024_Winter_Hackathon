4926f8ead688ae2b0ba0bfbcf12dea662e6974b2
hoonzz07 <wjdwogns0070120@gmail.com>
2024-01-05 06:44:08 +0900

    middle_padding

--- js/mainIndex.js
+++ js/mainIndex.js
@@ -1,27 +1,26 @@
-const $hamburger = document.querySelector('.hamburger');
-const $sideMenu = document.querySelector('.sideMenu');
-const $blackOut = document.querySelector('.blackout');
-const $gnb = document.querySelector('.gnb');
+const $hamburger = document.querySelector(".hamburger");
+const $sideMenu = document.querySelector(".sideMenu");
+const $blackOut = document.querySelector(".blackout");
+const $gnb = document.querySelector(".gnb");
 
 // 햄버거매뉴 클릭 시 aside형식으로 나오게 만들기
-$hamburger.addEventListener('click', function(){
-    $sideMenu.classList.add('open')
-    $blackOut.classList.add('open')
+$hamburger.addEventListener("click", function () {
+  $sideMenu.classList.add("open");
+  $blackOut.classList.add("open");
 
-    document.body.style.overflow = 'hidden'
-})
+  document.body.style.overflow = "hidden";
+});
 
 // 다른곳 클릭시 사이드 네비게이션 닫기
-$blackOut.addEventListener('click', function(){
-    $sideMenu.classList.remove('open')
-    $blackOut.classList.remove('open')
+$blackOut.addEventListener("click", function () {
+  $sideMenu.classList.remove("open");
+  $blackOut.classList.remove("open");
 
-    document.body.style.overflow = 'auto'
-})
+  document.body.style.overflow = "auto";
+});
 
 function disableScroll() {
-    if ($sideMenu.classList.contains('open')) {
-        document.addEventListener('scroll', )
-    }
+  if ($sideMenu.classList.contains("open")) {
+    document.addEventListener("scroll");
+  }
 }
-
