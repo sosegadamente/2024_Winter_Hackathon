ee368c370ff99cf264ee9432403d5d81b785ed27
matempong <>
2024-01-04 19:33:32 +0900

    add

--- js/mainIndex.js
+++ js/mainIndex.js
@@ -1,4 +1,3 @@
-
 const $hamburger = document.querySelector('.hamburger');
 const $sideMenu = document.querySelector('.sideMenu');
 const $blackOut = document.querySelector('.blackout');
@@ -6,10 +5,23 @@ const $gnb = document.querySelector('.gnb');
 
 // 햄버거매뉴 클릭 시 aside형식으로 나오게 만들기
 $hamburger.addEventListener('click', function(){
-    // console.log(1);
     $sideMenu.classList.add('open')
-    $blackOut.classList.add('open')    
-    // setTimeout(() => {
-    //     $blackOut.classList.add('open')
-    // }, 750);
+    $blackOut.classList.add('open')
+
+    document.body.style.overflow = 'hidden'
 })
+
+// 다른곳 클릭시 사이드 네비게이션 닫기
+$blackOut.addEventListener('click', function(){
+    $sideMenu.classList.remove('open')
+    $blackOut.classList.remove('open')
+
+    document.body.style.overflow = 'auto'
+})
+
+function disableScroll() {
+    if ($sideMenu.classList.contains('open')) {
+        document.addEventListener('scroll', )
+    }
+}
+
