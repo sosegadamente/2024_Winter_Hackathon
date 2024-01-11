ebd33555599150a6ec629a0a9bfa223f386c2a00
hoonzz07 <wjdwogns0070120@gmail.com>
2024-01-11 23:30:29 +0900

    todo_middle_commit

--- js/todo.js
+++ js/todo.js
@@ -1,26 +1,24 @@
-const modal = document.querySelector(".modal")
-const open_modal = document.querySelector(".button")
-const close_modal = document.querySelector(".close_modal")
+const modal = document.querySelector(".modal");
+const open_modal = document.querySelector(".button");
+const close_modal = document.querySelector(".close_modal");
 
-open_modal.onclick = function() {
-  modal.style.display = "block"; 
-}
+open_modal.onclick = function () {
+  modal.style.display = "block";
+};
 
-close_modal.onclick = function()
-{
+close_modal.onclick = function () {
   modal.style.display = "none";
-}
+};
 
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none"; // 모달 숨기기
   }
-}
+};
 
-const todoInputValue = document.querySelector('.modal_input');
-const plusBtn = document.querySelector('.plusBtn');
+const todoInputValue = document.querySelector(".modal_input");
+const plusBtn = document.querySelector(".plusBtn");
 
-plusBtn.addEventListener('click', function(){
-  const createTodoList = document.createElement('li');
-})
-
+plusBtn.addEventListener("click", function () {
+  const createTodoList = document.createElement("li");
+});
